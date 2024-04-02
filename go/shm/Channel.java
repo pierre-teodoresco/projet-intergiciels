343304b61251f8a61ca3834f88847684f2edc552
Pierre Teodoresco <p.teodoresco@gmail.com>
2024-04-02 15:22:29 +0200

    Channel memoire partagée

--- go/shm/Channel.java
+++ go/shm/Channel.java
@@ -3,28 +3,55 @@ package go.shm;
 import go.Direction;
 import go.Observer;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Channel<T> implements go.Channel<T> {
+    private String name;
+    private T message;
+    private boolean isMessageAvailable; // true if message is available to read false otherwise
+    private Map<Direction, Observer> observers;
 
     public Channel(String name) {
-        // TODO
+        this.name = name;
+        isMessageAvailable = false;
+        observers = new HashMap<>();
+    }
+
+    public synchronized void out(T v) {
+        while (isMessageAvailable) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        message = v;
+        isMessageAvailable = true;
+        notifyAll();
     }
     
-    public void out(T v) {
-        // TODO
-    }
-    
-    public T in() {
-        // TODO
-        return null;
+    public synchronized T in() {
+        while (!isMessageAvailable) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return null; // or handle differently
+            }
+        }
+        T message = this.message;
+        isMessageAvailable = false;
+        notifyAll();
+        return message;
     }
 
     public String getName() {
-        // TODO
-        return null;
+        return name;
     }
 
     public void observe(Direction dir, Observer observer) {
-        // TODO
+        observers.put(dir, observer);
     }
         
 }
