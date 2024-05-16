cc09c8d5b01f9f9eb85aa4e934dacf8e8c465857
ecolomb <ecolomb@inp-toulouse.fr>
2024-05-16 14:04:12 +0200

    modif qui marche pas bien

--- go/test/TestShm13.java
+++ go/test/TestShm13.java
@@ -12,7 +12,7 @@ public class TestShm13 {
     }
 
     public static void main(String[] a) {
-        Logger.setDebug(false);
+        Logger.setDebug(true);
 
         Factory factory = new go.shm.Factory();
         Channel<Integer> c1 = factory.newChannel("c1");
