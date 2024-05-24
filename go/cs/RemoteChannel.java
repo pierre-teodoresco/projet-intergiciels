d1978a6f90f022784e23e05b9070804506a4eb01
pierre-teodoresco <p.teodoresco@gmail.com>
2024-05-24 10:50:53 +0200

    remove useless code

--- go/cs/RemoteChannel.java
+++ go/cs/RemoteChannel.java
@@ -7,14 +7,9 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface RemoteChannel<T> extends Remote {
-
-    public void out(T v) throws RemoteException;
-
-    public T in() throws RemoteException;
-
-    public String getName() throws RemoteException;
-
-    public void observe(Direction direction, Observer observer) throws RemoteException;
-
-    public go.Channel<T> getShmChannel() throws RemoteException;
+    void out(T v) throws RemoteException;
+    T in() throws RemoteException;
+    String getName() throws RemoteException;
+    void observe(Direction direction, Observer observer) throws RemoteException;
+    go.Channel<T> getShmChannel() throws RemoteException;
 }
